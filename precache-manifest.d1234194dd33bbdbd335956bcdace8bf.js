99ae65cc3a7607981868122430260026c1ea5d4a
Innocent1312 <anthonydev33@gmail.com>
2019-12-03 14:16:15 +0200

    Updates

---
+++ precache-manifest.d1234194dd33bbdbd335956bcdace8bf.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cce72d6b4be83c1f96bc3d6385aa674e",
+    "url": "/react_static-list-of-todos/index.html"
+  },
+  {
+    "revision": "d1bf606ea68b30240b50",
+    "url": "/react_static-list-of-todos/static/js/2.a7975bab.chunk.js"
+  },
+  {
+    "revision": "3736519337a3e7976775",
+    "url": "/react_static-list-of-todos/static/js/main.ab608365.chunk.js"
+  },
+  {
+    "revision": "ea6f31786c2aed313835",
+    "url": "/react_static-list-of-todos/static/js/runtime~main.00529491.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_static-list-of-todos/precache-manifest.1460eb5f6aa52629add7e99c8f3a4e44.js"
+  "/react_static-list-of-todos/precache-manifest.d1234194dd33bbdbd335956bcdace8bf.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react_static-list-of-todos/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
